143157664f8c467c91c2c510f727568456492dde
aniketkute <kuteaniket666@gmail.com>
2023-12-07 00:17:37 +0530

    Revert "Merge pull request #1 from bhumixa/main"
    This reverts commit 7898621b8a9a66d9d04d7f345fede21ceb459146, reversing
    changes made to bdaaa191de02d8e7890b4f041d3e884ed2ff1fe0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { ScanmeComponent } from './pages/scanme/scanme.component';
+import { CheckBalanceComponent } from './pages/check-balance/check-balance.component';
+import { RechargeComponent } from './pages/recharge/recharge.component';
+import { WithdrawalComponent } from './pages/withdrawal/withdrawal.component';
+
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    NgbModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent,
+    ScanmeComponent,
+    CheckBalanceComponent,
+    RechargeComponent,
+    WithdrawalComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
